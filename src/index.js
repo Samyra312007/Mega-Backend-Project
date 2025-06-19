ba2aaa446d4bf2bd6a4e7d752d0a5a88381878e1
Samayra312007 <samayra312007@gmail.com>
2025-06-19 16:14:32 +0530

    add response, error and configure cors

--- src/index.js
+++ src/index.js
@@ -9,28 +9,3 @@ connectToDatabase();
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
-
-/*
-import express from "express"
-
-const app = express()
-
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("ERRR: ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, () =>{
-            console.log(`App is listening on port ${process.env.PORT}`)
-        })
-    } catch (error) {
-        console.error("ERROR: ", error)
-        throw error
-    }
-})()
-
-*/
